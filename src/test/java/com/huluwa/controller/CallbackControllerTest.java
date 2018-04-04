45542b47c2c3b96a78958a454e5da7bc95616037
Cai Jiang <luffy.ja@gmail.com>
2018-04-04 23:45:30 +0800

    优化回调测试。

--- src/test/java/com/huluwa/controller/CallbackControllerTest.java
+++ src/test/java/com/huluwa/controller/CallbackControllerTest.java
@@ -3,13 +3,15 @@ package com.huluwa.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.huluwa.config.PaymentSpringConfig;
+import com.huluwa.event.CallbackSuccess;
 import me.jiangcai.lib.test.SpringWebTest;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import java.io.IOException;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,12 +23,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class CallbackControllerTest extends SpringWebTest {
 
+    static CallbackSuccess lastEvent = null;
+
     ///
+    @Configuration
+    public static class Config {
+        @EventListener(CallbackSuccess.class)
+        public void getIt(CallbackSuccess event) {
+            lastEvent = event;
+        }
+    }
 
     @Test
     public void transferResult() throws Exception {
+        testSuccessWithOrder("0aab2");
+        testSuccessWithOrder("0a10s211");
+    }
+
+    private void testSuccessWithOrder(String orderId) throws Exception {
+        lastEvent = null;
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode infoEntity = mapper.readTree(new ClassPathResource("/success.json").getFile());
+        JsonNode infoEntity = mapper.readTree(new ClassPathResource("/success_with_" + orderId + ".json").getFile());
         JsonNode requestEntity = infoEntity.get("request");
 
         mockMvc.perform(post("/huluwa/transfer")
@@ -34,10 +51,10 @@ public class CallbackControllerTest extends SpringWebTest {
                 .content(requestEntity.get("content").asText())
         )
                 .andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(content().string("SUCCESS"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("SUCCESS"))
         ;
-
-
+        Assert.assertNotNull("确保收到了成功事件", lastEvent);
+        Assert.assertEquals("收到的成功支付事件id是正确的", orderId, lastEvent.getOrderId());
     }
 }
